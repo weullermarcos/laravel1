e22e88260d1b126b9716bf437447a511e1925db1
Weuller Marcos <weullermarcos@hotmail.com>
2020-12-24 10:53:01 -0300

    criando layouts

--- resources/views/admin/config.blade.php
+++ resources/views/admin/config.blade.php
@@ -1,15 +1,25 @@
-<h1> CONFIG </h1>
 
-<p>Meu nome é {{$nome}}</p>
-<p>Minha idade é {{$idade}}</p>
+@extends('layouts.admin')
 
-<form method="POST">
-    @csrf
-    <br/> Nome: <input type="text" name="nome">
-    <br/> Idade: <input type="text" name="idade">
-    <br/> Nome: <input type="submit" value="Enviar">
-</form>
+@section('title', 'configurações')
+
+@section('conteudo')
+
+    <h1> CONFIG </h1>
+
+    <p>Meu nome é {{$nome}}</p>
+    <p>Minha idade é {{$idade}}</p>
+
+    <form method="POST">
+        @csrf
+        <br/> Nome: <input type="text" name="nome">
+        <br/> Idade: <input type="text" name="idade">
+        <br/> Nome: <input type="submit" value="Enviar">
+    </form>
 
 
-<a href="/config/info"> Informações </a>
+    <a href="/config/info"> Informações </a>
 
+    <p>{{$nomeProjeto}} - Versão: {{$versao}}</p>
+
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+
+    <title>@yield('title') - Laravel 1</title>
+
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <section>
+        @yield('conteudo')
+    </section>
+
+    <footer>
+        Rodapé...
+    </footer>
+
+</body>
+
+</html>
